451956cf91c482f5f5a5340128636980eaf5dfc4
MariahAdams <mariah.adams23@yahoo.com>
2018-09-04 07:48:52 -0700

    adding single and photogrid components

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -8,6 +8,7 @@ const Main = React.createClass({
         <h1>
           <Link to="/">ReduxStagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
     );
   }
---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="grid-photo">
+        I am the photo grid
+      </div>
+    );
+  }
+})
+
+export default PhotoGrid;
