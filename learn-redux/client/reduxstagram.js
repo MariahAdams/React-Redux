7d13bbf9a27d2aa97bb9d969a6bcf68fc4177e97
MariahAdams <mariah.adams23@yahoo.com>
2018-09-04 08:06:24 -0700

    created redux store

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import css from './styles/style.styl';
+
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
-
-import css from './styles/style.styl';
 
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 
