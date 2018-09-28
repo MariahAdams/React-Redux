5f08526cce1eb0e6f439cf3a32364127710ded44
MariahAdams <mariah.adams23@yahoo.com>
2018-09-28 15:36:14 -0700

    fixed bug in index.js

--- learn-redux/client/reducers/index.js
+++ learn-redux/client/reducers/index.js
@@ -3,6 +3,6 @@ import { routerReducer } from 'react-router-redux';
 import posts from '../reducers/posts';
 import comments from '../reducers/comments';
 
-const rootReducer = combineReducers({posts, comments, router: routerReducer });
+const rootReducer = combineReducers({posts, comments, routing: routerReducer });
 
 export default rootReducer;
