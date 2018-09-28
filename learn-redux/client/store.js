91c1bed84088ca79808a8a6d904b6703a5d4344a
MariahAdams <mariah.adams23@yahoo.com>
2018-09-28 15:25:43 -0700

    added action creators

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -2,7 +2,6 @@ import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
-//import root reducer
 import rootReducer from './reducers/index';
 
 import comments from './data/comments';
