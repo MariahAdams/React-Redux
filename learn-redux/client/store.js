ae01ec6c5210361eb59ad3148b94c1694eea2e83
MariahAdams <mariah.adams23@yahoo.com>
2018-09-29 12:04:25 -0700

    enabled hot reloading

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -19,4 +19,11 @@ const store = createStore(
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
+if(module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
 export default store;
